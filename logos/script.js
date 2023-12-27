3e1409b1887de80a714b60ca0bc91e1070a12d9a
barbara <91904482+brbnoriega@users.noreply.github.com>
2023-12-27 20:54:35 -0300

    queryselectorall

--- logos/script.js
+++ logos/script.js
@@ -20,3 +20,26 @@ colorPicker.forEach((item) => {
       input.value = color;
     });
   });
+
+  const customDropdown = document.querySelectorAll(".custom-dropdown");
+
+customDropdown.forEach((item) => {
+    //seleccionamos todas las opciones del customdropdown
+    options = item.querySelectorAll(".option");
+
+    options.forEach((option) => {
+        //evento para todas las opciones
+      option.addEventListener("click", () => {
+        //seleccionamos todas las opciones del elemento padre para remover la clase activa
+        allOptions = option.parentElement.querySelectorAll(".option");
+        allOptions.forEach((item) => {
+          item.classList.remove("active");
+        });
+        option.classList.add("active");
+        //refresh con el texto seleccionado
+        item.querySelector(".selected").innerHTML = option.innerHTML;
+        generateQRCode();
+      });
+    });
+  });
+  
